61cc77c45ccd12ed41b1bd25b547f089dd90efce
Taras <example@gmail.com>
2022-04-17 13:09:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("arr" + " - object");
+console.log("4" + 9);
+
+let incr = 10,
+   decr = 10;
+incr++;
+decr--;
+console.log(++incr);
+console.log(--decr);
+console.log(7%2);
